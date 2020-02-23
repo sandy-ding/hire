b0e44f0e531d32d53c4feba840fd79e1cddadfe3
Sandy <dhl15821939286@gmail.com>
2020-02-23 20:45:08 +0800

    lint

--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,18 @@ export interface ErrorMessage {
 const PARSE_ERROR_REGULAR_EXPRESSION = /[a-zA-Z]+:\/\/(.\/)*.+\.([a-zA-Z]+)(:[\d]+){2}$/gm;
 
 export function parseError(err: Error): ErrorMessage {
-              const parseStack =
-                err.stack && err.stack.match(PARSE_ERROR_REGULAR_EXPRESSION);
-              const errorMessage: ErrorMessage = {
-                message: err.message,
-                stack: []
-              };
-  if (!!parseStack) {
+  const parseStack = err.stack && err.stack.match(PARSE_ERROR_REGULAR_EXPRESSION);
+  const errorMessage: ErrorMessage = {
+    message: err.message,
+    stack: [],
+  };
+  if (parseStack) {
     parseStack.forEach((item: string) => {
-      const message = item.split(":");
+      const message = item.split(':');
       errorMessage.stack.push({
         column: Number(message.pop()),
         line: Number(message.pop()),
-        filename: message.join(":")
+        filename: message.join(':'),
       });
     });
   }
